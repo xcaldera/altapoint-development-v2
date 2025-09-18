f575d9553edd2b5a612769e3b81e258feacc3252
xcaldera <xcaldera.JC@gmail.com>
2025-09-18 13:25:03 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['irp-cdn.multiscreensite.com', 'irp.cdn-website.com'],
+  },
+}
+
+module.exports = nextConfig
